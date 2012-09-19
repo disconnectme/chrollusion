cdf58efa05bc21a51aef66dbdbbc9f81642de1fd
Gary Teh <garyjob@Garys-MacBook-Air.local>
2012-09-18 18:04:21 -0700

    sample analytics

---
+++ chrollusion.safariextension/chrome/scripts/analytics.js
@@ -0,0 +1,28 @@
+// Usage : 
+var analytics = function() {
+  var self = this;
+  var d = new Date();
+  self.sessionId = d.getTime();
+}
+
+analytics.prototype.triggerEvent = function(event, attr) {
+  var self = this;
+  attr = attr || {};
+  var url = 'http://artariteenageriot.disconnect.me:9080/' + event + '/' + self.sessionId;
+  if (Object.keys(attr).length > 0) {
+    queryString = '';
+    for(key in attr) {
+      queryString += key + '=' + attr[key] + '&'
+    }
+  }
+  url += '/?' + queryString;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      //console.log(xhr.responseText);
+    }    
+  }; // Implemented elsewhere.
+  xhr.open("GET", url, true);
+  xhr.send();
+}
